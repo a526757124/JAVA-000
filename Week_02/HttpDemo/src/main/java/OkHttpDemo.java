dd4da76b84eda9864034bb50db5c9b34735e4152
billy <lijun526757124@163.com>
2020-10-29 10:12:33 +0800

    第二次作业

---
+++ Week_02/HttpDemo/src/main/java/OkHttpDemo.java
@@ -0,0 +1,20 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class OkHttpDemo {
+    public static void main(String[] args) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://localhost:8801")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
